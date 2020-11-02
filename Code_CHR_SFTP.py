9f42dd20b0904ddb51a1b669165eb796ed32b162
Radmehr.A <radmehraskarnia@gmail.com>
2020-11-02 16:46:24 +0330

    Update Code_CHR_SFTP.py
    Connection to SFTP and downloading existing files.
    Note: IP Address and Logchangedin credentials are

--- Code_CHR_SFTP.py
+++ Code_CHR_SFTP.py
@@ -7,20 +7,12 @@ Created on Wed Apr 15 12:25:55 2020
 
 import os
 import pysftp
-#import re
 
-#import gzip
-#with gzip.open('/export/home/omc/var/fileint/TSNBI/UMTS_PCHR/20200419/MSRNCH05') as f:
-#file_content = f.read()
-#from xml.dom import minidom
-
-#print(os.getcwd())
-#print("File List: ")
 cnopts = pysftp.CnOpts()
 cnopts.hostkeys = None    # disable host key checking.
 
 
-sftp = pysftp.Connection(host='10.220.211.115', username='ftpuser', password='Changeme_123', cnopts=cnopts)
+sftp = pysftp.Connection(host='11.111.111.111', username='FTP', password='ABCD', cnopts=cnopts)
 sftp.cwd('/export/home/omc/var/fileint/TSNBI/UMTS_PCHR/20200609/MSRNCH05')
 #regex = re.compile(r'([A-Z]{1}\d{4})',re.IGNORECASE)
 for attr in sftp.listdir_attr():
@@ -30,7 +22,6 @@ data = []
 for i in sftp.listdir():
     #if regex.findall(i) != []:
         data.append(i)
-#folder_list = sftp.listdir_attr()
     
 file1 = open(r"D:\CHR\Huawei\CHR\Sites.txt",'w') # Open file to write H LTE
 
